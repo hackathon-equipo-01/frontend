b5ec8887308fa6e79ee4195b88166d1161332676
Nuclea88 <ingrid.lopov@gmail.com>
2026-02-19 23:26:47 +0100

    to mrge

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,6 @@ import btnContenedores from "./images/image4.png";
 
 
 export default function Home() {
-<<<<<<< HEAD
     const { login } = useAuth();
     const navigate = useNavigate();
 
@@ -173,24 +172,3 @@ return (
         </div>
     );
 }
-=======
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#FFFBEB]">
-      <h1 className="text-5xl font-black text-green-600 mb-8 uppercase tracking-tighter drop-shadow-sm">
-        ECO-MONSTRUOS
-      </h1>
-      
-      <Link 
-        to="/juego" 
-        className="px-10 py-5 bg-green-500 text-white rounded-full font-bold shadow-xl hover:bg-green-600 transition-all hover:scale-105 active:scale-95 text-xl border-b-4 border-green-700"
-      >
-        ¡EMPEZAR A RECICLAR!
-      </Link>
-      
-      <p className="mt-6 text-gray-500 font-medium italic">
-        Ayuda a nuestros amigos a clasificar sus residuos
-      </p>
-    </div>
-  );
-}
->>>>>>> bb3eed24cd28c4993bd5418eb4fb547080bca8f8
