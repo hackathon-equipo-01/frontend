b161db765064bb9c60d372b03f4cf01c74bfdc8f
Cristina Viejó <krissvinti@gmail.com>
2026-02-19 23:51:28 +0100

    changed

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
 import { DroppableZone } from './DroppableZone';
 import { DraggableItem } from './DraggableItem';
+import bg from "../images/metrics_bg.png";
 
 import { getAllContainers, processWaste } from '../services/ContainersCall.jsx';
 
@@ -72,7 +73,15 @@ async function handleDragEnd(event) {
 
     return (
         <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
-            <div className="min-h-screen bg-[#FFFBEB] p-4 flex flex-col items-center">
+            <div
+  className="min-h-screen w-full p-4 flex flex-col items-center"
+  style={{
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  }}
+>
                 <div className="flex gap-4 mb-20">
                     {containers.map((monster) => (
                         <DroppableZone key={monster.id} id={monster.id}>
